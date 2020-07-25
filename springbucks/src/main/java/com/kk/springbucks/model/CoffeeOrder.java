e742aa486c27dcb6c5be462a32f98d1c65f05d21
kaikanwu <wukaikan@gmail.com>
2020-07-25 15:12:24 +0800

    update entity

--- springbucks/src/main/java/com/kk/springbucks/model/CoffeeOrder.java
+++ springbucks/src/main/java/com/kk/springbucks/model/CoffeeOrder.java
@@ -5,46 +5,50 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.joda.money.Money;
 
 import javax.persistence.*;
-import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author kaikanwu
- * @date 25/08/2019
+ * created on 7/25/2020
  */
-@Entity
-@Table(name = "T_MENU")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Coffee implements Serializable {
+@Table("T_ORDER")
+@Entity
+@Builder
+public class CoffeeOrder {
+
 
     @Id
     @GeneratedValue
     private Long id;
 
 
-    private String name;
+    private String customer;
 
 
-    @Column
-    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyAmount",
-    parameters = {@org.hibernate.annotations.Parameter(name = "currencyCode", value = "CNY")})
-    private Money price;
+
+    @ManyToMany
+    @JoinTable("T_ORDER_COFFEE")
+    private List<Coffee> items;
 
 
-    @Column(updatable = false)
+
+    @Column(nullable = false)
+    private Integer state;
+
     @CreationTimestamp
+    @Column(updatable = false)
     private Date createTime;
 
 
     @UpdateTimestamp
     private Date updateTime;
+
 
 }
